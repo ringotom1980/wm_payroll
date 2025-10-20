88505b62102856351610796b221fcf33c0707e87
ringotom1980 <ringo_tom2007@yahoo.com.tw>
2025-10-20 21:35:06 +0800

    員工主檔加載全站JS

--- Public/modules/employees.php
+++ Public/modules/employees.php
@@ -229,7 +229,7 @@
             </div>
         </div>
     </div>
-
+    <script src="/assets/js/app.js?v=13" defer></script>
     <script src="/assets/js/employees.js?v=8" defer></script>
 </body>
 

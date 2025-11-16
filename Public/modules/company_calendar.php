acbda8f706681c502711c1377dcce55802214e8e
ringotom1980 <ringo_tom2007@yahoo.com.tw>
2025-11-16 09:55:49 +0800

    修改JS快顯

--- Public/modules/company_calendar.php
+++ Public/modules/company_calendar.php
@@ -94,6 +94,6 @@ require __DIR__ . '/../partials/header.php';
 
   <?php require __DIR__ . '/../partials/footer.php'; ?>
   <script src="/assets/js/company_calendar.js?v=3" defer></script>
-  <script src="/assets/js/leave_records.js?v=14" defer></script>
+  <script src="/assets/js/leave_records.js?v=15" defer></script>
 </body>
 </html>
